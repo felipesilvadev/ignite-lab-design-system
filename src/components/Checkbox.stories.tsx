5e3ac562954c4f3f2a9c384a906efcfbe6e73a93
Felipe Silva <felipe.silva@assesi.com>
2022-10-13 16:01:52 -0300

    configurando tailwind e componentes storybook

---
+++ src/components/Checkbox.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox, CheckboxRootProps } from './Checkbox';
+import { Text } from './Text';
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => {
+      return (
+        <div className="flex items-center gap-2">
+          {Story() }
+
+          <Text size="sm">Lembrar-me de mim por 30 dias</Text>
+        </div>
+      );
+    }
+  ],
+} as Meta<CheckboxRootProps>;
+
+export const Default: StoryObj<CheckboxRootProps> = {};
